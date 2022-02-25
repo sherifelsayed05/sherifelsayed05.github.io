fe3a57780fd129992ee34567770c0471ebb57bde
sherifelsayed05 <97370241+sherifelsayed05@users.noreply.github.com>
2022-02-24 18:38:30 -0800

    Add files via upload

---
+++ ISpyGame/iSpy.js
@@ -0,0 +1,14 @@
+
+item.style.textDecoration="line-through";
+
+var mickyMouse = document.getElementById("mickey" ,
+mickyMouse.addEventListener("click", function(){
+    foundItem("mickey");
+}, false);
+                                         
+
+                                         
+ function replay(){
+location.reload();
+}   
+
