e5137e9b9ec86ba2c7c51bce0e5d077dc5f8cca8
sherifelsayed05 <97370241+sherifelsayed05@users.noreply.github.com>
2022-02-26 22:27:15 -0800

    Update Slideshow.js

--- Slideshow/Slideshow.js
+++ Slideshow/Slideshow.js
@@ -1,7 +1,7 @@
-var myImages =["https://www.imms.org/images/dolphin_presentation/dolphin1.jpg", "http://cdn3.theeventchronicle.com/wp-content/uploads/2014/09/dolphin-intelligence.jpg","http://www.liveanimalslist.com/mammals/images/dolphin-in-water.jpg","http://i1151.photobucket.com/albums/o632/bowietvc15/Bowietvc15-2/DolphinsWallpaper2.jpg","http://i1151.photobucket.com/albums/o632/bowietvc15/funny%20gifs/animal-art-cute-dolphin-nature-Favimcom-311780.jpg"];
+var myImages =["Capture.PNG", "Capture2.PNG","Capture3.PNG","Capture4.PNG","Capture5.PNG"];
 
 
-var captionImages =["Laugh","Love","Lunch","Happiness","Family"];
+var captionImages =["Customize your rhombus","Put dots anywhere","Create your grocery list","Learn your multiplication","Create an account"];
 
  var index=0; 
 
