867422224905fd1decdb2baa0dd23bda6ca227f1
sherifelsayed05 <97370241+sherifelsayed05@users.noreply.github.com>
2022-03-12 11:42:38 -0800

    Update Multiply.js

--- Module5/Multiply.js
+++ Module5/Multiply.js
@@ -1,34 +1,29 @@
              // Unit of table
 
-operator = GetValueFromUser("operator");
-table = GetValueFromUser("table")
 
-// Write the message into the page
-var el = document.getElementById('blackboard');
-el.innerHTML = GetTableContent(operator, table);
+             table = GetValueFromUser("table")
 
-function GetValueFromUser(valueType) {
-  var greetingMessage = 'Hello. How are you?';
-  if (valueType == "operator") {
-    greetingMessage += "Enter Multiply";
-  }
-  else
-  {
-    greetingMessage += "Enter a number";
-  }
-  return prompt(greetingMessage);
-}
+             // Write the message into the page
+             var el = document.getElementById('blackboard');
+             el.innerHTML = GetTableContent(table);
 
-function GetTableContent(operator, table) {
-     var i = 1;                 // Set counter to 1
-  var msg = '';              // Message  
-  
-  {
-    //Do multiplication
-    while (i < 11) {
-      msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
-      i++;
-    }
-  }
-  return msg;
-}
+             function GetValueFromUser(valueType) {
+               var message = "Enter a number";
+               return prompt(message);
+             }
+
+
+
+             function GetTableContent(table) {
+               var i = 1; // Set counter to 1
+               var msg = ''; // Message  
+
+               {
+                 //Do multiplication
+                 while (i < 11) {
+                   msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+                   i++;
+                 }
+               }
+               return msg;
+             }
